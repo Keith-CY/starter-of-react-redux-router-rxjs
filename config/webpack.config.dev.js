86029f477a55c0f8b674c9c3212b9b1777a1dc57
Keith <keithwhisper@gmail.com>
2017-09-06 14:55:18 +0800

    Add Named Modules Plugin

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -36,6 +36,7 @@ const devConfig = {
   devtool: 'source-map',
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: JSON.stringify('development'),
